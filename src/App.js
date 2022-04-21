478961226c52aada91a7b0afbd034022216dd67b
shantatei <teirenxuanshanta@gmail.com>
2022-04-21 16:57:07 +0800

    finished 1/3 of checkoutform (address info)

--- src/App.js
+++ src/App.js
@@ -1,51 +1,81 @@
-import React , {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce';
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
 
-import {Products, Navbar} from './components';
+import { Products, Navbar, Cart , Checkout} from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 const App = () => {
 
-  const [products, setProducts] =  useState([]);
-  const [cart, setCart] =useState ({});
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
-  const fetchProducts = async () =>{
-    const {data} = await commerce.products.list();
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
     setProducts(data);
 
   }
 
-  const fetchCart = async () =>{
-     setCart(await commerce.cart.retrieve());
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
 
   }
 
-  const handleAddToCart = async(productId, quantity)=>{
+  const handleAddToCart = async (productId, quantity) => {
 
-    const item= await commerce.cart.add(productId, quantity)
+    const {cart} = await commerce.cart.add(productId, quantity)
 
 
-    setCart(item.cart);
+    setCart(cart);
   }
 
-  useEffect(() =>{
+  const handleUpdateCartQty = async (productId, quantity) => {
+
+    const {cart} = await commerce.cart.update(productId, {quantity});
+
+    setCart(cart)
+  }
+
+  const handleRemoveFromCart = async (productId) =>{
+    const {cart} = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () =>{
+
+    const {cart} = await commerce.cart.empty();
+
+    setCart(cart)
+  }
+
+  useEffect(() => {
 
     fetchProducts();
     fetchCart();
 
-  },[]);
+  }, []);
 
-  console.log(cart)
 
   return (
-    <div>
-      <Navbar totalItems = {cart.total_items} />
-      <Products products = {products} onAddToCart={handleAddToCart} />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route  path="/" element = {<Products products={products} onAddToCart={handleAddToCart} />}> </Route>
+          <Route  path="/cart" element ={<Cart cart={cart}  
+          handleRemoveFromCart ={handleRemoveFromCart}
+          handleEmptyCart = {handleEmptyCart}
+          handleUpdateCartQty  ={handleUpdateCartQty}
+          />}></Route>
+          <Route path="/checkout" element={<Checkout cart ={cart}/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
